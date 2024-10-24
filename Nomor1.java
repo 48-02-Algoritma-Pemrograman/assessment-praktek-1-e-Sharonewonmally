55ac1205e35087cde7a747e15ff62793f725c6ea
Sharone j.p wonmally <sharonejpwonmally@gmail.com>
2024-10-24 14:40:52 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Nomor1{
     public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+        int jumlahanggota;
+        double tunjangan;
+        double totaltunjangan;
 
+            System.out.print("input jumlah anggota keluarga : ");
+            jumlahanggota =scanner.nextInt();
+            
+            System.out.print("masukan tunjangan anggota keluarga");
+            tunjangan = scanner.nextDouble();
+
+            for (int i = 1; i <= jumlahanggota; i++){
+                System.out.println("anggota keluarga ke-" + i + ":");
+
+                String tunjangan1 = ""; 
+                String tunjangan2 = ""; 
+                String tunjangan3 = "";
+
+
+                switch (jumlahanggota) {
+                    case 1:
+                        tunjangan = 100000;
+                        break;
+                    case 2:
+                        tunjangan = 50000;
+                        break;
+                    case 3:
+                        tunjangan = 50000;
+                        break;
+                    
+                    }
+                        double ip = jumlahanggota /tunjangan;
+
+                        System.out.println("jumlahtunjangan" +tunjangan);
+
+                        scanner.close();
+                }    
+        }
+    }
     }
 }
